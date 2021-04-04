b207980de6ab17fd7b359dc4527a902fe99e16aa
dobbi_free <gangb6444@gmail.com>
2021-04-04 13:07:03 +0300

    7-rewrite-Header-on-func

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -3,14 +3,10 @@ import Header from "./Header";
 import {connect} from "react-redux";
 import {LogoutUserThunkCreator} from "../../redux/auth-reducer";
 
-class HeaderContainer extends React.Component {
-
-
-    render() {
-        return (
-            <Header {...this.props}/>
-        );
-    }
+const  HeaderContainer = (props) => {
+    return (
+        <Header {...props}/>
+    );
 }
 
 const mapStateToProps = (state) => ({
