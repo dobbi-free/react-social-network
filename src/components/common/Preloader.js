a1855eb466ddab096e66fe7c2f43a4b63cba371d
Yevgen Poberezhnyy <65887924+emergencyXX@users.noreply.github.com>
2021-06-08 15:14:22 +0300

    Update Preloader.js

--- src/components/common/Preloader.js
+++ src/components/common/Preloader.js
@@ -1,7 +1,7 @@
 const Preloader = () => {
     return(
         <div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/54/Ajux_loader.gif"/>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/54/Ajux_loader.gif" alt=""/>
         </div>
     )
 }
